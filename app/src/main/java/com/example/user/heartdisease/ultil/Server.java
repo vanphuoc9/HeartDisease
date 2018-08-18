a079b1bcadb3054ddbfff541ea1dc4b00b3cb4f3
vanphuoc9 <thaiphuoc1997@gmail.com>
2018-08-18 11:07:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/user/heartdisease/ultil/Server.java
@@ -0,0 +1,9 @@
+package com.example.user.heartdisease.ultil;
+
+/**
+ * Created by User on 17/08/2018.
+ */
+
+public class Server {
+    public static String getResult = "http://172.20.10.2:5000/getResult";
+}
