d71cb92914728a3a06dd03742e7c2183cd54f5e1
vanphuoc9 <thaiphuoc1997@gmail.com>
2018-08-25 11:56:01 +0700

    heart disease

--- app/src/main/java/com/example/user/heartdisease/activity/QuestionPart2Activity.java
+++ app/src/main/java/com/example/user/heartdisease/activity/QuestionPart2Activity.java
@@ -11,5 +11,6 @@ public class QuestionPart2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_part2);
+        //
     }
 }
