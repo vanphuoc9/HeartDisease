935408c2ad095e91042da8eca36a312252dd7254
vanphuoc9 <thaiphuoc1997@gmail.com>
2018-08-25 11:52:35 +0700

    heart disease

---
+++ app/src/main/java/com/example/user/heartdisease/activity/QuestionPart2Activity.java
@@ -0,0 +1,15 @@
+package com.example.user.heartdisease.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.heartdisease.R;
+
+public class QuestionPart2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_part2);
+    }
+}
